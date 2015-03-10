ce5d40995deb9fc837f55a7b706cd87eae827f51
Joseph Guadagno <jguadagno@hotmail.com>
2015-03-09 17:18:02 -0700

    Updated to jQuery Mobile 1.4.5 and fixed the broken references.

--- verifycdn.js
+++ verifycdn.js
@@ -1,10 +1,10 @@
 (window.jQuery || document.write('<script src="/backup/jquery-2.1.1.min.js"><\/script>'));
-(window.jQuery.mobile || document.write('<script src="/backup/jquery.mobile-1.4.3.min.js"><\/script>'));
+(window.jQuery.mobile || document.write('<script src="/backup/jquery.mobile-1.4.5.min.js"><\/script>'));
 $.each(document.styleSheets, function(i,sheet){
-  if(sheet.href=='http://code.jquery.com/mobile/1.4.9/jquery.mobile-1.4.3.min.css') {
+  if(sheet.href=='http://code.jquery.com/mobile/1.4.9/jquery.mobile-1.4.5.min.css') {
     var rules = sheet.rules ? sheet.rules : sheet.cssRules;
     if (rules === null || rules.length == 0) {
-      $('<link rel="stylesheet" type="text/css" href="/backup/jquery.mobile-1.4.3.min.css" />').appendTo('head');
+      $('<link rel="stylesheet" type="text/css" href="/backup/jquery.mobile-1.4.5.min.css" />').appendTo('head');
     }
  }
 });
