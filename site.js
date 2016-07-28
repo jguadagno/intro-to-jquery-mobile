55c3deb0d8694c21272ce4fbb680628176942368
jguadagno <jguadagno@hotmail.com>
2016-07-27 18:57:22 -0700

    AppInsights Update

--- site.js
+++ site.js
@@ -1,5 +1,4 @@
-<script type="text/javascript">
-  var appInsights=window.appInsights||function(config){
+var appInsights=window.appInsights||function(config){
     function r(config){t[config]=function(){var i=arguments;t.queue.push(function(){t[config].apply(t,i)})}}var t={config:config},u=document,e=window,o="script",s=u.createElement(o),i,f;s.src=config.url||"//az416426.vo.msecnd.net/scripts/a/ai.0.js";u.getElementsByTagName(o)[0].parentNode.appendChild(s);try{t.cookie=u.cookie}catch(h){}for(t.queue=[],i=["Event","Exception","Metric","PageView","Trace","Dependency"];i.length;)r("track"+i.pop());return r("setAuthenticatedUserContext"),r("clearAuthenticatedUserContext"),config.disableExceptionTracking||(i="onerror",r("_"+i),f=e[i],e[i]=function(config,r,u,e,o){var s=f&&f(config,r,u,e,o);return s!==!0&&t["_"+i](config,r,u,e,o),s}),t
     }({
         instrumentationKey:"ba0141c2-73a8-4302-97cd-8c83e8d588ca"
@@ -7,4 +6,3 @@
        
     window.appInsights=appInsights;
     appInsights.trackPageView();
-</script>
