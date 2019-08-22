e74e7d33392b2538972c1a210171d668b8d2c242
Joseph Guadagno <jguadagno@hotmail.com>
2019-08-21 19:01:28 -0700

    Updated AI Key

--- site.js
+++ site.js
@@ -1,7 +1,7 @@
 var appInsights=window.appInsights||function(config){
     function r(config){t[config]=function(){var i=arguments;t.queue.push(function(){t[config].apply(t,i)})}}var t={config:config},u=document,e=window,o="script",s=u.createElement(o),i,f;s.src=config.url||"//az416426.vo.msecnd.net/scripts/a/ai.0.js";u.getElementsByTagName(o)[0].parentNode.appendChild(s);try{t.cookie=u.cookie}catch(h){}for(t.queue=[],i=["Event","Exception","Metric","PageView","Trace","Dependency"];i.length;)r("track"+i.pop());return r("setAuthenticatedUserContext"),r("clearAuthenticatedUserContext"),config.disableExceptionTracking||(i="onerror",r("_"+i),f=e[i],e[i]=function(config,r,u,e,o){var s=f&&f(config,r,u,e,o);return s!==!0&&t["_"+i](config,r,u,e,o),s}),t
     }({
-        instrumentationKey:"a724e669-702b-4db9-b07b-c3004f88444e"
+        instrumentationKey:"9ba12fc8-eff5-4671-9458-673d465a9297"
     });
        
     window.appInsights=appInsights;
