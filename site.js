868375be82b609ff65e2ad06007d27bda6ed77f1
jguadagno <jguadagno@hotmail.com>
2023-03-02 17:33:49 -0700

    Added app insights

--- site.js
+++ site.js
@@ -1,7 +1,7 @@
 var appInsights=window.appInsights||function(config){
     function r(config){t[config]=function(){var i=arguments;t.queue.push(function(){t[config].apply(t,i)})}}var t={config:config},u=document,e=window,o="script",s=u.createElement(o),i,f;s.src=config.url||"//az416426.vo.msecnd.net/scripts/a/ai.0.js";u.getElementsByTagName(o)[0].parentNode.appendChild(s);try{t.cookie=u.cookie}catch(h){}for(t.queue=[],i=["Event","Exception","Metric","PageView","Trace","Dependency"];i.length;)r("track"+i.pop());return r("setAuthenticatedUserContext"),r("clearAuthenticatedUserContext"),config.disableExceptionTracking||(i="onerror",r("_"+i),f=e[i],e[i]=function(config,r,u,e,o){var s=f&&f(config,r,u,e,o);return s!==!0&&t["_"+i](config,r,u,e,o),s}),t
     }({
-        instrumentationKey:"ab06d448-f663-4414-92ec-4ac1dcec5d25"
+        instrumentationKey:"a9d14844-37c3-449b-a8c6-4da9c6b2123e"
     });
        
     window.appInsights=appInsights;
